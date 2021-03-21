2e46584fdba9691770aa47ee16b02945abfe4669
isysolo <ismail.solombrino@lernende.bbw.ch>
2021-03-21 12:24:50 +0100

    Create React App Embeded our own Video "IPv4-Lernvideo" CSS styling

---
+++ multimedia/src/App.js
@@ -0,0 +1,12 @@
+import ReactPlayer from "./ReactPlayer";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+      <ReactPlayer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ multimedia/src/ReactPlayer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+export default function () {
+    return (
+        < div>
+
+            < ReactPlayer
+                controls
+                url='https://www.youtube.com/watch?v=y9MCHwZNjpk&ab_channel=NicolaB%C3%A4chi' />
+
+        < /div>
+    )
+
+}
